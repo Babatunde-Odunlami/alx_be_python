976c7cf750987dafebeffc215dab885fc6d4504c
Babatunde-Odunlami <Odunlamibabatunde@outlook.com>
2024-09-14 13:47:40 -0700

    correction to basic operations task 0

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,10 +1,9 @@
-# using basic operations in python with number1 and number2
-
+#Using basic operations in python with number1 and number2
 number1 = 10
 number2 = 5
 addition = number1 + number2
-subtracton = number1 - number2
+subtraction = number1 - number2
 multiplication = number1 * number2
-print("Addition of ", number1, " and ", number2, " is ", addition)
+print("Addition of ", number1, " aind ", number2, " is ", addition)
 print("Subtraction of ", number1, " and ", number2, " is ", subtraction)
 print("Multiplication of ", number1, " and ", number2, " is ", multiplication)
