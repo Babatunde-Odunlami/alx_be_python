fcc016272448c05f4176cfe697a1f2bf9005cf2b
Babatunde-Odunlami <Odunlamibabatunde@outlook.com>
2024-09-22 12:34:37 -0700

    correction to control flow tasks

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,5 +2,5 @@
 number = int(input("Enter a number to see its multiplication table:"))
 for x in range(1,11):
     z = number * x 
-    print(x, " * ", number, " = ", z)
+    print(f"{number} * {x} ={z}")
 
