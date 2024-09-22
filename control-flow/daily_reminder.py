fd6159c6bdcff6053aa87fec3cf244cd0d151bbb
Babatunde-Odunlami <Odunlamibabatunde@outlook.com>
2024-09-22 13:26:43 -0700

    correction to control flow tasks

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -2,12 +2,15 @@
 task = input("Enter your task:").proper()
 priority = input("Priority (high/medium/low):").lower()
 time_bound = input("Is it time-bound? (yes/no):").lower()
+
+customised_message1 = "that requires immediate attention today!"
+customised_message2 = "Consider completing it when you have free time."
 match priority:
     case "high":
         if time_bound == "yes":
-            print(f "Reminder: {task} is a {priority} priority task that requires immediate attention today")
+            print(f "Reminder: {task} is a {priority} priority task {customised_message}")
         else:
-            print(f "Note: {task} is a {_priority} priority task. Consider completing it when you have free time.")
+            print(f "Note: {task} is a {priority} priority task. {customised_message2}")
         break;
     case _:
         print("Please follow the prompt's instructions")
