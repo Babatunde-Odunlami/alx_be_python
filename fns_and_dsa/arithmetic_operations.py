199a27ec4dad60ad6d8d00fafd4cd26273e7133a
Babatunde-Odunlami <Odunlamibabatunde@outlook.com>
2024-09-29 11:28:46 -0700

    correcting arithmetic operations task

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -8,20 +8,21 @@ operation = (
 #define a function to for the various arithmetic operations
 def perform_operation(num1, num2, operation):
     """function to perform arithmetic operation stated"""
+    result = None  #Initialize the reult variable
     if operation == "add":
-        result = num1 + num2
+        result = num1+num2
     elif operation == "subtract":
-        result = num1 - num2
+        result = num1-num2
     elif operation == "multiply":
-        result = num1 * num2
+        result = num1*num2
     elif operation == "divide":
-        if num2==0:
-            print("enter valid numbers as arguments")
+        if num2 == 0:
+            return "enter valid numbers as arguments"
         else:
             result = num1 / num2
     else:
-        print("please enter a valid operation")
-    print(f"Result: {result}")
-
+        return "please enter a valid operation"
+    return f"Result: {result}"
 
 result = perform_operation(num1, num2, operation)
+print(result)
