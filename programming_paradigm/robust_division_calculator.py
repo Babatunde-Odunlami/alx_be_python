0fe9e1bc4b7599fb12237cd0896090b2f4a221ea
Babatunde-Odunlami <Odunlamibabatunde@outlook.com>
2024-10-06 13:38:58 -0700

    oop concept task 1

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,9 +1,9 @@
 #OOP concepts: creating a robust division calculator
 def safe_divide(numerator, denominator):
     try:
-        if numerator.isnumeric() and denominator.isnumeric():
+        if float(numerator).isnumeric() and float(denominator).isnumeric():
             if denominator != 0:
-                answer = numerator/denominato)
+                answer = numerator/denominator
                 return f"The result of the division is {answer:.1f}"
             else:
                 raise ZeroDivisionError("Error: Cannot divide by zero.")
